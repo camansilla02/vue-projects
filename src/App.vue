00177e67bf34686176809144625d724577d40b56
Chris <66217579+camansilla02@users.noreply.github.com>
2024-04-14 22:07:28 -0400

    Upload branch

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="title">
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+         <button @click="handleClick('salary')">order by salary</button>
+         <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+   <JobsList :jobs="jobs" :order="order" />
+    </div>
+</template>
+
+<script  lang="ts">
+import { defineComponent, ref } from 'vue';
+import type {Job} from './types/Job';
+import type {OrderTerm} from  './types/OrderTerm';
+import JobsList from './components/JobsList.vue';
+
+export default defineComponent ({
+  name: 'App',
+  components: {JobsList},
+  setup(){
+     // const state = reactive({
+     //   name: 'Link',
+     //   age:25 as string | number
+
+    //  })
+    // return { ...toRefs(state)  }
+    //const name = ref('Link')
+    //const age = ref<number | string>(25)
+    
+    //return {name, age}
+    const jobs = ref<Job[]>([
+      {title: 'farm worker', location: 'Lon lon ranch', salary: 30000, id: '1' },
+      {title: 'quaryyman', location: 'Death mountain', salary: 40000, id: '2' },
+      {title: 'flute player', location: 'The Lost woods', salary: 35000, id: '3' },
+      {title: 'fisherman', location: 'Lake Hylia', salary: 21000, id: '4' },
+      {title: 'prison guard', location: 'Gerudo valley', salary: 32000, id: '5' }
+    ])
+    
+    const order =ref<OrderTerm>('title')
+    const handleClick = (term: OrderTerm) =>{
+            order.value = term
+    }
+      return {jobs,handleClick, order}
+
+  },
+
+});
+</script>
+
+<style scoped>
+.app{
+  background-color: gainsboro;
+}
+
+header {
+  text-align: center;
+}
+
+header .order{
+  margin-top: 20px;
+}
+
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+header .title{
+  display: flex;
+  justify-content: center;
+
+}
+
+header img{
+  width: 60px;
+  margin-right: 20px;
+}
+
+header h1{
+  font-size: 3em;
+}
+
+</style>
